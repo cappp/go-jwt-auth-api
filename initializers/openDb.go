0b8867da75f2f1b08749985c7d3061f6d5f1d739
Cap <caapppp@outlook.com>
2024-03-01 18:12:48 -0300

    Update openDb.go

--- initializers/openDb.go
+++ initializers/openDb.go
@@ -3,14 +3,20 @@ package initializers
 import (
 	"database/sql"
 	"log"
+	"os"
 )
 
 var DB *sql.DB
 
 func OpenDb() {
+	os.Remove("./users.db")
 	db, err := sql.Open("sqlite3", "./users.db")
 	if err != nil {
 		log.Fatal("Erro ao abrir o banco de dados:\n" + err.Error())
+	}
+	_, err = db.Exec("create table users (name text, username text, password text)")
+	if err != nil {
+		log.Fatal("Erro ao criar a tabela 'users':\n" + err.Error())
 	}
 	DB = db
 }
